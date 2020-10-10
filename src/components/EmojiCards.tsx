013a67b3897a59538ecdee9cb3b5db75810b00e6
Akash Rajpurohit <akashrajpurohit.009@gmail.com>
2020-10-10 15:51:07 +0530

    :art: search functionality

--- src/components/EmojiCards.tsx
+++ src/components/EmojiCards.tsx
@@ -1,68 +1,108 @@
-import { useEffect, useReducer, useCallback } from 'react';
+import React, { useEffect, useReducer, useCallback } from 'react';
 import { IEmoji } from '../interfaces/IEmoji';
 import { Card } from './Card';
 import { Intersection } from './Intersection';
+import { Box, Input, Stack, Flex, Text } from '@chakra-ui/core';
 
 interface IEmojiCardProps {
 	emojis: IEmoji[];
 }
 
 type State = {
-  totalEmojis: IEmoji[];
-  emojis: IEmoji[];
-  emojisLength: number;
-}
+	totalEmojis: IEmoji[];
+	emojis: IEmoji[];
+	filteredEmojis: IEmoji[];
+	emojisLength: number;
+};
 
-type Action =
- | { type: 'LOAD_MORE' }
- | { type: 'SEARCH', search: string };
+type Action = { type: 'SEARCH' | 'LOAD_MORE'; payload?: string };
 
 const emojiReducer = (state: State, action: Action): State => {
-  const { type } = action;
-  const { emojisLength, totalEmojis } = state;
+	const { type, payload } = action;
+	const { emojisLength, totalEmojis, filteredEmojis } = state;
 
-  switch(type) {
-    case 'LOAD_MORE': {
-      const newLength = emojisLength + 40;
+	switch (type) {
+		case 'LOAD_MORE': {
+			const newLength = Math.max(40, Math.min(emojisLength + 40, filteredEmojis.length));
+
+			return {
+				...state,
+				emojis: filteredEmojis.slice(0, newLength),
+				emojisLength: newLength,
+			};
+    }
+    
+    case 'SEARCH': {
+      let searchTerm = payload!.toLowerCase();
+
+      if(searchTerm.trim() === '') {
+        return {
+          ...state,
+					emojis: totalEmojis.slice(0, emojisLength),
+					filteredEmojis: totalEmojis
+        };
+      }
+
+      const searchResults = totalEmojis.filter(({ code }) => code.toLowerCase().includes(searchTerm));
+
       return {
-        ...state,
-        emojis: totalEmojis.slice(0, newLength),
-        emojisLength: newLength
+				...state,
+				emojis: searchResults,
+				filteredEmojis: searchResults
       }
     }
 
-    default: {
-      return {
-        ...state
-      }
-    }
-  }
-}
+		default: {
+			return {
+				...state,
+			};
+		}
+	}
+};
 
 export const EmojiCards = ({ emojis }: IEmojiCardProps) => {
-  
-  const initialState = {
-    totalEmojis: emojis,
-    emojis: [] as IEmoji[],
-    emojisLength: 0
-  }
+	const initialState = {
+		totalEmojis: emojis,
+		filteredEmojis: emojis,
+		emojis: [] as IEmoji[],
+		emojisLength: 0,
+  };
 
-  const [state, dispatch] = useReducer(emojiReducer, initialState);
+	const [state, dispatch] = useReducer(emojiReducer, initialState);
 
-  useEffect(() => {
-    dispatch({ type: 'LOAD_MORE' });
+	useEffect(() => {
+		dispatch({ type: 'LOAD_MORE' });
+	}, []);
+
+	const loadMore = useCallback(() => {
+		dispatch({ type: 'LOAD_MORE' });
   }, []);
-
-  const loadMore = useCallback(() => {
-    dispatch({ type: 'LOAD_MORE' });
-  }, []);
   
+  const filterOnSearch = useCallback((value: string) => {
+    dispatch({ type: 'SEARCH', payload: value });
+  }, [])
+
 	return (
-		<>
-			{state.emojis.map((e) => (
-				<Card code={e.code} img={e.img} key={e.code} />
-			))}
-      <Intersection onVisible={() => loadMore()} />
-		</>
+		<Stack pt='8' w='70%'>
+			<Box mb='4'>
+				<Input placeholder='Search Shortcodes' onChange={(e) => {
+          filterOnSearch(e.target.value);
+        }} />
+			</Box>
+			<Flex
+				flexWrap='wrap'
+				w='100%'
+				justifyContent='center'
+				alignItems='center'
+			>
+				{state.emojis.map((e) => (
+					<Card code={e.code} img={e.img} key={e.code} />
+				))}
+				{
+					state.emojis.length === 0 && <Text my="8">No Emoji Found for this search</Text>
+				}
+				<Intersection onVisible={() => loadMore()} />
+			</Flex>
+		</Stack>
 	);
 };
