411ed8c232a1790462c36d0501e636db5809c8cb
Akash Rajpurohit <akashrajpurohit.009@gmail.com>
2020-10-12 12:58:16 +0530

    :zap: cache data on api level to reduce req call to github api :smile_cat:

--- src/pages/api/get-emojis.ts
+++ src/pages/api/get-emojis.ts
@@ -1,9 +1,25 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async (_: NextApiRequest, res: NextApiResponse) => {
+let cache: Record<string, string>;
+let cachedAt: number;
+
+const cacheTimeLimit = 1000 * 60 * 60; // 1 hour
+
+const getData = async (): Promise<Record<string, string>> => {
+  if (cache && Date.now() - cachedAt < cacheTimeLimit) return cache;
+
   const response = await fetch('https://api.github.com/emojis');
 
   const data = await response.json();
+
+  cache = data;
+  cachedAt = Date.now();
+
+  return cache;
+}
+
+export default async (_: NextApiRequest, res: NextApiResponse) => {
+  const data = await getData();
 
   return res.status(200).json(data);
 };
